57c36bc509eb5ae04effbefb663883109efbef05
Deepam Wagh <deepam.wagh@gmail.com>
2025-12-02 20:28:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
+const list = document.getElementById("infi-list");
 
+for (let i = 1; i <= 10; i++) {
+  const li = document.createElement("li");
+  li.innerText = "Item " + i;
+  list.appendChild(li);
+}
+
+let count = 10;
+
+list.addEventListener("scroll", function () {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    for (let i = 1; i <= 2; i++) {
+      count++;
+      const li = document.createElement("li");
+      li.innerText = "Item " + count;
+      list.appendChild(li);
+    }
+  }
+});
